debe413486c9ada4fa7f8c4e7f6704d5e14a3be9
bitsy <bitsydoge@proton.me>
2025-11-23 01:36:28 +0100

    feat: update number dump output to include expected values

--- main.c
+++ main.c
@@ -92,7 +92,7 @@ void testNumber() {
 
 	$LET numInt16 =  BCNumberCreate((int16_t)-4500);
 	FAIL_IF_NOT(BCNumberGetType(numInt16) == BCNumberTypeInt16);
-	$LET numUInt16 =  BCNumberCreate((uint16_t)255);
+	$LET numUInt16 =  BCNumberCreate((uint16_t)6500);
 	FAIL_IF_NOT(BCNumberGetType(numUInt16) == BCNumberTypeUInt16);
 
 	$LET numInt32 =  BCNumberCreate((int32_t)-15500);
@@ -129,34 +129,34 @@ void testNumber() {
 	BCAutorelease($OBJ numBool);
 
 	printf("Numbers Dump: \n");
-	printf("Int8: %s\n", TO_STR(numInt8));
-	printf("UInt8: %s\n", TO_STR(numUInt8));
-	printf("Int16: %s\n", TO_STR(numInt16));
-	printf("UInt16: %s\n", TO_STR(numUInt16));
-	printf("Int32: %s\n", TO_STR(numInt32));
-	printf("UInt32: %s\n", TO_STR(numUInt32));
-	printf("Int64: %s\n", TO_STR(numInt64));
-	printf("UInt64: %s\n", TO_STR(numUInt64));
-	printf("Float: %s\n", TO_STR(numFloat));
-	printf("Double: %s\n", TO_STR(numDouble));
-	printf("Int: %s\n", TO_STR(numInt));
-	printf("Bool: %s\n", TO_STR(numBool));
+	printf("Int8: %s, Expect: -64\n", TO_STR(numInt8));
+	printf("UInt8: %s, Expect: 255\n", TO_STR(numUInt8));
+	printf("Int16: %s, Expect: -4500\n", TO_STR(numInt16));
+	printf("UInt16: %s, Expect: 6500\n", TO_STR(numUInt16));
+	printf("Int32: %s, Expect: -15500\n", TO_STR(numInt32));
+	printf("UInt32: %s, Expect: 135255\n", TO_STR(numUInt32));
+	printf("Int64: %s, Expect: -9223372036854775807\n", TO_STR(numInt64));
+	printf("UInt64: %s, Expect: 18446744073709551615\n", TO_STR(numUInt64));
+	printf("Float: %s, Expect: 3.14f\n", TO_STR(numFloat));
+	printf("Double: %s, Expect: -5.123456789\n", TO_STR(numDouble));
+	printf("Int: %s, Expect: 42\n", TO_STR(numInt));
+	printf("Bool: %s, Expect: true\n", TO_STR(numBool));
 
 	int32_t valInt = 0;
 	BCNumberGetValue(numInt, &valInt);
-	printf("Extracted Int: %d\n", valInt);
+	printf("Extracted Int: %d, Expect: 42\n", valInt);
 
 	float valFloat = 0;
 	BCNumberGetValue(numInt, &valFloat);
-	printf("Extracted float: %f\n", valFloat);
+	printf("Extracted float: %f, Expect: 42.0000\n", valFloat);
 
 	int64_t valInt64 = 0;
 	BCNumberGetValue(numDouble, &valInt64);
-	printf("Extracted int64_t: %zd\n", valInt64);
+	printf("Extracted int64_t: %zd, Expect: -5\n", valInt64);
 
 	double valDouble = 0;
 	BCNumberGetValue(numDouble, &valDouble);
-	printf("Extracted double: %lf\n", valDouble);
+	printf("Extracted double: %lf, Expect: -5.123456\n", valDouble);
 
 	// Equality
 	const BCNumberRef numInt2 = BCNumberCreate(42);
