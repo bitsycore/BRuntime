28595c955657e854dbaa35c591c9df4ece9884d4
Bitsy <bitsydoge@proton.me>
2025-11-24 23:50:26 +0100

    feat: add Autorelease hot ref

--- BCRuntime/BCAutoreleasePool.c
+++ BCRuntime/BCAutoreleasePool.c
@@ -10,31 +10,34 @@
 // =========================================================
 
 typedef struct BCAutoreleasePool {
-	struct BCAutoreleasePool* next;
-	struct BCAutoreleasePool* parent;
-	uint8_t count;
-	BCObjectRef stack[CAPACITY];
+  struct BCAutoreleasePool *next;
+  struct BCAutoreleasePool *parent;
+  struct BCAutoreleasePool *hot;
+  uint8_t count;
+  BCObjectRef stack[CAPACITY];
 } BCAutoreleasePool;
 
 $TLS BCAutoreleasePool gRootPool = {
-	.next = NULL,
-	.parent = NULL,
-	.count = 0,
-	.stack = 0,
+    .next = NULL,
+    .parent = NULL,
+    .hot = NULL,
+    .count = 0,
+    .stack = {},
 };
 
-$TLS BCAutoreleasePool* CurrentAutoReleasePool = NULL;
+$TLS BCAutoreleasePool *CurrentAutoReleasePool = NULL;
 
 // =========================================================
 // MARK: Private
 // =========================================================
 
-static BCAutoreleasePool* AllocNewPool() {
-	BCAutoreleasePool* pool = BCMalloc(sizeof(BCAutoreleasePool));
-	pool->count = 0;
-	pool->parent = NULL;
-	pool->next = NULL;
-	return pool;
+static BCAutoreleasePool *AllocNewPool() {
+  BCAutoreleasePool *pool = BCMalloc(sizeof(BCAutoreleasePool));
+  pool->count = 0;
+  pool->parent = NULL;
+  pool->next = NULL;
+  pool->hot = pool;
+  return pool;
 }
 
 // =========================================================
@@ -42,52 +45,50 @@ static BCAutoreleasePool* AllocNewPool() {
 // =========================================================
 
 void BCAutoreleasePoolPush(void) {
-	if (CurrentAutoReleasePool == NULL) {
-		CurrentAutoReleasePool = &gRootPool;
-		gRootPool.count = 0;
-		gRootPool.next = NULL;
-	}
-	else {
-		BCAutoreleasePool* pool = AllocNewPool();
-		pool->parent = CurrentAutoReleasePool;
-		CurrentAutoReleasePool = pool;
-	}
+  if (CurrentAutoReleasePool == NULL) {
+    CurrentAutoReleasePool = &gRootPool;
+    gRootPool.count = 0;
+    gRootPool.next = NULL;
+    gRootPool.hot = &gRootPool;
+  } else {
+    BCAutoreleasePool *pool = AllocNewPool();
+    pool->parent = CurrentAutoReleasePool;
+    CurrentAutoReleasePool = pool;
+  }
 }
 
 void BCAutoreleasePoolPop(void) {
-	if (!CurrentAutoReleasePool) return;
-	BCAutoreleasePool* pool = CurrentAutoReleasePool;
-	CurrentAutoReleasePool = pool->parent;
-	while (pool) {
-		for (uint8_t i = 0; i < pool->count; i++) {
-			BCRelease(pool->stack[i]);
-		}
-		BCAutoreleasePool* next = pool->next;
-		if (pool != &gRootPool) BCFree(pool);
-		pool = next;
-	}
+  if (!CurrentAutoReleasePool)
+    return;
+  BCAutoreleasePool *pool = CurrentAutoReleasePool;
+  CurrentAutoReleasePool = pool->parent;
+  while (pool) {
+    for (uint8_t i = 0; i < pool->count; i++) {
+      BCRelease(pool->stack[i]);
+    }
+    BCAutoreleasePool *next = pool->next;
+    if (pool != &gRootPool)
+      BCFree(pool);
+    pool = next;
+  }
 }
 
 BCObjectRef BCAutorelease(const BCObjectRef obj) {
-	if (!obj) return NULL;
-	if (!CurrentAutoReleasePool) {
-		fprintf(stderr, "Warning: Autorelease with no pool. Leaking.\n");
-		return obj;
-	}
+  if (!obj)
+    return NULL;
+  if (!CurrentAutoReleasePool) {
+    fprintf(stderr, "Warning: Autorelease with no pool. Leaking.\n");
+    return obj;
+  }
 
-	BCAutoreleasePool* pool = CurrentAutoReleasePool;
-	if (pool->count == CAPACITY) {
-		do{
-			if (pool->next == NULL) {
-				BCAutoreleasePool* newPool = AllocNewPool();
-				pool->next = newPool;
-				pool = newPool;
-			} else {
-				pool = pool->next;
-			}
-		} while (pool->count == CAPACITY);
-	}
+  BCAutoreleasePool *pool = CurrentAutoReleasePool->hot;
+  if (pool->count == CAPACITY) {
+    BCAutoreleasePool *newPool = AllocNewPool();
+    pool->next = newPool;
+    CurrentAutoReleasePool->hot = newPool;
+    pool = newPool;
+  }
 
-	pool->stack[pool->count++] = obj;
-	return obj;
+  pool->stack[pool->count++] = obj;
+  return obj;
 }
