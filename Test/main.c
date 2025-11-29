bf3f6fbadfc3109b3e8cfd6df144d9af9e1e9f5d
bitsy <bitsydoge@proton.me>
2025-11-29 22:14:56 +0100

    feat: add demo function showcasing autorelease and collection usage

--- Test/main.c
+++ Test/main.c
@@ -5,6 +5,67 @@
 
 int RETRY = 1;
 
+void demo(void) {
+	BCAutoreleaseScope() {
+
+		$LET str = $("Boxed");
+		$LET strAutorelease = $$("Boxed Autoreleased");
+
+		$LET formatedStr = $("String with construction %d", 123);
+		$LET stringInPool = BCStringPooledLiteral("String in global pool");
+
+
+		$LET list = $LIST("List Demo", 1, 2, 3, 4, 5);
+		$$LIST("Autoreleased list demo");
+
+		// $$LIST are autoreleased and $LIST push and pop an autorelease pool during it's creation allowing to free inner created objects
+		// so use $$LIST when creating list directly inside a list
+		$$LIST(
+			"List with inner list",
+			$$LIST(
+				"Inner Autoreleased List",
+				"A",
+				"B",
+				$$LIST("Inner Autoreleased List", "A", "B", "C")
+			)
+		);
+
+		$LET map = $MAP("Key1", "Value1", "Key2", "Value2", "Key3", "Value3");
+		$$MAP("Autoreleased map", "demo");
+
+		$$MAP( // $$MAP work like $$LIST
+			"Autoreleased map with inner map", "val",
+			"InnerMap1", $$MAP("InnerKey1", "InnerValue1", "InnerKey2", "InnerValue2"),
+			"InnerMap2", $$MAP("InnerKeyA", "InnerValueA", "InnerKeyB", "InnerValueB")
+		);
+
+		const char* buffer = BCAllocatorAlloc(kBCAllocatorRefSystem, 4096);
+		BCArenaRef arena = BCArenaCreate(NULL, buffer, 4096);
+		const BCAllocatorRef allocator = BCArenaAllocator(arena);
+
+		$LET sb = BCStringBuilderCreate(allocator);
+		BCAutorelease($OBJ sb);
+		BCStringBuilderAppend(sb, "Hello from SB");
+		BCStringBuilderAppendChar(sb, ' ');
+		BCStringBuilderAppendChar(sb, '!');
+		BCStringBuilderAppendChar(sb, ' ');
+		BCStringBuilderAppendString(sb, str);
+		BCStringBuilderAppendChar(sb, ' ');
+		BCStringBuilderAppendFormat(sb, "Number: %f", 3.141);
+		$LET buildedStr = BCStringBuilderFinish(sb);
+
+		printf("Str: %s", BCStringCPtr(buildedStr));
+
+		BCAutoreleaseAll(
+			$OBJ str,
+			$OBJ formatedStr,
+			$OBJ list,
+			$OBJ map,
+			$OBJ buildedStr
+		);
+	}
+}
+
 int BCMain() {
 	BCObjectDebugSetEnabled(BC_true);
 	BCObjectDebugSetKeepFreed(BC_false);
@@ -21,6 +82,8 @@ int BCMain() {
 			testStringBuilder();
 			testReleasePool();
 			testClassRegistry();
+
+			demo();
 
 			BCObjectDebugDump();
 			BCStringPoolDebugDump();
