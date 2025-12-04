6bb8a3cd002be22ed16ed74157775a159f177b06
Bitsy <bitsydoge@proton.me>
2025-12-04 21:56:12 +0100

    style: reformat BCBytesArray.c & BCString.c

--- BCRuntime/Object/BCBytesArray.c
+++ BCRuntime/Object/BCBytesArray.c
@@ -1,10 +1,11 @@
 #include "BCBytesArray.h"
 
-#include <string.h>
-
 #include "BCObject.h"
+#include "BCStringBuilder.h"
 #include "../Core/BCClass.h"
 #include "../Struct/BCRange.h"
+
+#include <string.h>
 
 // =========================================================
 // MARK: Struct
@@ -17,12 +18,6 @@ typedef struct BCBytesArray {
 } BCBytesArray;
 
 // =========================================================
-// MARK: Class
-// =========================================================
-
-#include "BCStringBuilder.h"
-
-// =========================================================
 // MARK: Class Methods
 // =========================================================
 
@@ -31,8 +26,8 @@ static BC_bool BCBytesArrayEqual(const BCObjectRef self, const BCObjectRef other
 	if (!self || !other) return BC_false;
 	if (self->cls != other->cls) return BC_false;
 
-	const BCBytesArrayRef a = (BCBytesArrayRef)self;
-	const BCBytesArrayRef b = (BCBytesArrayRef)other;
+	const BCBytesArrayRef a = (BCBytesArrayRef) self;
+	const BCBytesArrayRef b = (BCBytesArrayRef) other;
 
 	if (a->count != b->count) return BC_false;
 
@@ -40,29 +35,30 @@ static BC_bool BCBytesArrayEqual(const BCObjectRef self, const BCObjectRef other
 }
 
 static uint32_t BCBytesArrayHash(const BCObjectRef self) {
-	const BCBytesArrayRef selfCast = (BCBytesArrayRef)self;
-    uint64_t hash = 5381;
-    for (size_t i = 0; i < selfCast->count; i++) {
-        hash = ((hash << 5) + hash) + selfCast->bytes[i];
-    }
-    return hash;
+	const BCBytesArrayRef selfCast = (BCBytesArrayRef) self;
+	uint64_t hash = 5381;
+	for (size_t i = 0; i < selfCast->count; i++) {
+		hash = (hash << 5) + hash + selfCast->bytes[i];
+	}
+	return hash;
 }
 
 static BCStringRef BCBytesArrayToString(const BCObjectRef self) {
-    const BCStringBuilderRef builder = BCStringBuilderCreate(NULL);
-	const BCBytesArrayRef selfCast = (BCBytesArrayRef)self;
-    BCStringBuilderAppendFormat(builder, "(%zu) ", selfCast->count);
-    for (size_t i = 0; i < selfCast->count; i++) {
-        BCStringBuilderAppendFormat(builder, "%02X", selfCast->bytes[i]);
-    }
-    const BCStringRef result = BCStringBuilderFinish(builder, BC_true);
+	const BCStringBuilderRef builder = BCStringBuilderCreate(NULL);
+	const BCBytesArrayRef selfCast = (BCBytesArrayRef) self;
+	if (selfCast->count == 0) BCStringBuilderAppendFormat(builder, "(0) <Empty>");
+	else BCStringBuilderAppendFormat(builder, "(%zu) 0x", selfCast->count);
+	for (size_t i = 0; i < selfCast->count; i++) {
+		BCStringBuilderAppendFormat(builder, "%02X", selfCast->bytes[i]);
+	}
+	const BCStringRef result = BCStringBuilderFinish(builder, BC_true);
 	BCRelease($OBJ builder);
 	return result;
 }
 
 static BCObjectRef BCBytesArrayCopy(const BCObjectRef self) {
-	const BCBytesArrayRef selfCast = (BCBytesArrayRef)self;
-    return (BCObjectRef)BCBytesArrayCreateWithBytes(selfCast->count, selfCast->bytes);
+	const BCBytesArrayRef selfCast = (BCBytesArrayRef) self;
+	return (BCObjectRef) BCBytesArrayCreateWithBytes(selfCast->count, selfCast->bytes);
 }
 
 // =========================================================
@@ -93,14 +89,14 @@ void ___BCINTERNAL___BytesArrayClassInit(void) {
 // =========================================================
 
 BCBytesArrayRef BCBytesArrayCreate(const size_t count) {
-	const BCBytesArrayRef arr = (BCBytesArrayRef)BCObjectAllocWithConfig(NULL, kBCBytesArrayClass.id, count * sizeof(uint8_t), BC_OBJECT_DEFAULT_FLAGS);
+	const BCBytesArrayRef arr = (BCBytesArrayRef) BCObjectAllocWithConfig(NULL, kBCBytesArrayClass.id, count * sizeof(uint8_t), BC_OBJECT_DEFAULT_FLAGS);
 	arr->count = count;
 	memset(arr->bytes, 0, count);
 	return arr;
 }
 
-BCBytesArrayRef BCBytesArrayCreateWithBytes(const size_t count, const uint8_t* bytes) {
-	const BCBytesArrayRef arr = (BCBytesArrayRef)BCObjectAllocWithConfig(NULL, kBCBytesArrayClass.id, count * sizeof(uint8_t), BC_OBJECT_DEFAULT_FLAGS);
+BCBytesArrayRef BCBytesArrayCreateWithBytes(const size_t count, const uint8_t *bytes) {
+	const BCBytesArrayRef arr = (BCBytesArrayRef) BCObjectAllocWithConfig(NULL, kBCBytesArrayClass.id, count * sizeof(uint8_t), BC_OBJECT_DEFAULT_FLAGS);
 	arr->count = count;
 	memcpy(arr->bytes, bytes, count);
 	return arr;
@@ -111,25 +107,25 @@ BCBytesArrayRef BCBytesArrayCreateWithBytes(const size_t count, const uint8_t* b
 // =========================================================
 
 void BCBytesArraySet(const BCBytesArrayRef arr, const size_t idx, const uint8_t byte) {
-    if (idx < arr->count) {
-        arr->bytes[idx] = byte;
-    }
+	if (idx < arr->count) {
+		arr->bytes[idx] = byte;
+	}
 }
 
 void BCBytesArraySetRange(const BCBytesArrayRef arr, BCRange range, uint8_t byte) {
 	if (!arr) return;
 	const size_t start = range.start;
-	const size_t len   = range.length;
+	const size_t len = range.length;
 	if (start > arr->count || len > arr->count - start) return;
 	memset(arr->bytes + start, byte, len);
 }
 
 void BCBytesArrayFill(const BCBytesArrayRef arr, const uint8_t byte) {
-    memset(arr->bytes, byte, arr->count);
+	memset(arr->bytes, byte, arr->count);
 }
 
 uint8_t BCBytesArrayGet(const BCBytesArrayRef arr, const size_t idx) {
-    if (idx >= arr->count) return 0; // Safety check
+	if (idx >= arr->count) return 0; // Safety check
 	return arr->bytes[idx];
 }
 
@@ -137,16 +133,16 @@ size_t BCBytesArrayCount(const BCBytesArrayRef arr) {
 	return arr->count;
 }
 
-uint8_t* BCBytesArrayBytes(const BCBytesArrayRef arr) {
+uint8_t *BCBytesArrayBytes(const BCBytesArrayRef arr) {
 	return arr->bytes;
 }
 
 int8_t BCBytesArrayCompare(const BCBytesArrayRef arr1, const BCBytesArrayRef arr2) {
-    const size_t minCount = arr1->count < arr2->count ? arr1->count : arr2->count;
-    const int cmp = memcmp(arr1->bytes, arr2->bytes, minCount);
-    if (cmp < 0) return -1;
-    if (cmp > 0) return 1;
-    if (arr1->count < arr2->count) return -1;
-    if (arr1->count > arr2->count) return 1;
-    return 0;
+	const size_t minCount = arr1->count < arr2->count ? arr1->count : arr2->count;
+	const int cmp = memcmp(arr1->bytes, arr2->bytes, minCount);
+	if (cmp < 0) return -1;
+	if (cmp > 0) return 1;
+	if (arr1->count < arr2->count) return -1;
+	if (arr1->count > arr2->count) return 1;
+	return 0;
 }
--- BCRuntime/Object/BCString.c
+++ BCRuntime/Object/BCString.c
@@ -16,10 +16,10 @@
 // =========================================================
 
 typedef struct BCString {
-  BCObject base;
-  BC_atomic_size length;
-  BC_atomic_uint_fast32 hash;
-  char *buffer;
+	BCObject base;
+	BC_atomic_size length;
+	BC_atomic_uint_fast32 hash;
+	char *buffer;
 } BCString;
 
 // =========================================================
@@ -27,33 +27,30 @@ typedef struct BCString {
 // =========================================================
 
 uint32_t StringHashImpl(const BCObjectRef obj) {
-  return BCStringHash((BCStringRef)obj);
+	return BCStringHash((BCStringRef) obj);
 }
 
 BC_bool StringEqualImpl(const BCObjectRef a, const BCObjectRef b) {
-  const BCStringRef s1 = (BCStringRef)a;
-  const BCStringRef s2 = (BCStringRef)b;
+	const BCStringRef s1 = (BCStringRef) a;
+	const BCStringRef s2 = (BCStringRef) b;
 
-  if (s1 == s2)
-    return BC_true;
-  if (BC_FLAG_HAS(s1->base.flags, BC_STRING_FLAG_POOLED) &&
-      BC_FLAG_HAS(s2->base.flags, BC_STRING_FLAG_POOLED))
-    return BC_false;
+	if (s1 == s2) return BC_true;
+	if (BC_FLAG_HAS(s1->base.flags, BC_STRING_FLAG_POOLED) &&
+	    BC_FLAG_HAS(s2->base.flags, BC_STRING_FLAG_POOLED))
+		return BC_false;
 
-  // Check Hash Cache
-  const uint32_t h1 = BC_atomic_load(&s1->hash);
-  const uint32_t h2 = BC_atomic_load(&s2->hash);
-  if (h1 != BC_HASH_UNSET && h2 != BC_HASH_UNSET && h1 != h2)
-    return BC_false;
+	// Check Hash Cache
+	const uint32_t h1 = BC_atomic_load(&s1->hash);
+	const uint32_t h2 = BC_atomic_load(&s2->hash);
+	if (h1 != BC_HASH_UNSET && h2 != BC_HASH_UNSET && h1 != h2) return BC_false;
 
-  if (BCStringLength(s1) != BCStringLength(s2))
-    return BC_false;
+	if (BCStringLength(s1) != BCStringLength(s2)) return BC_false;
 
-  return memcmp(s1->buffer, s2->buffer, BCStringLength(s1)) == 0;
+	return memcmp(s1->buffer, s2->buffer, BCStringLength(s1)) == 0;
 }
 
 BCStringRef StringToStringImpl(const BCObjectRef obj) {
-  return (BCStringRef)BCRetain(obj);
+	return (BCStringRef) BCRetain(obj);
 }
 
 BCObjectRef StringCopyImpl(const BCObjectRef obj) { return BCRetain(obj); }
@@ -62,18 +59,20 @@ BCObjectRef StringCopyImpl(const BCObjectRef obj) { return BCRetain(obj); }
 // MARK: Class
 // =========================================================
 
-static BCClass kBCStringClass = {.name = "BCString",
-                                 .dealloc = NULL,
-                                 .hash = StringHashImpl,
-                                 .equal = StringEqualImpl,
-                                 .toString = StringToStringImpl,
-                                 .copy = StringCopyImpl,
-                                 .allocSize = sizeof(BCString)};
+static BCClass kBCStringClass = {
+	.name = "BCString",
+	.dealloc = NULL,
+	.hash = StringHashImpl,
+	.equal = StringEqualImpl,
+	.toString = StringToStringImpl,
+	.copy = StringCopyImpl,
+	.allocSize = sizeof(BCString)
+};
 
 BCClassId BCStringClassId() { return kBCStringClass.id; }
 
 void ___BCINTERNAL___StringInitialize(void) {
-  BCClassRegistryInsert(&kBCStringClass);
+	BCClassRegistryInsert(&kBCStringClass);
 }
 
 // =========================================================
@@ -81,98 +80,105 @@ void ___BCINTERNAL___StringInitialize(void) {
 // =========================================================
 
 typedef struct StringPoolNode {
-  BCStringRef str;
-  struct StringPoolNode *next;
+	BCStringRef str;
+	struct StringPoolNode *next;
 } StringPoolNode;
 
 static struct {
-  BC_SPINLOCK_MAYBE(lock);
-  StringPoolNode *buckets[BC_STRING_POOL_SIZE];
+	BC_SPINLOCK_MAYBE(lock);
+	StringPoolNode *buckets[BC_STRING_POOL_SIZE];
 } StringPool;
 
 void ___BCINTERNAL___StringPoolInitialize(void) {
-  BCSpinlockInit(&StringPool.lock);
-  memset(StringPool.buckets, 0, sizeof(StringPool.buckets));
+	BCSpinlockInit(&StringPool.lock);
+	memset(StringPool.buckets, 0, sizeof(StringPool.buckets));
 }
 
 void ___BCINTERNAL___StringPoolDeinitialize(void) {
-  BCSpinlockDestroy(&StringPool.lock);
-  for (size_t i = 0; i < BC_STRING_POOL_SIZE; i++) {
-    const StringPoolNode *node = StringPool.buckets[i];
-    while (node) {
-      const StringPoolNode *next = node->next;
-      BCFree(node->str);
-      node = next;
-      if (next == NULL)
-        StringPool.buckets[i] = NULL;
-    }
-  }
+	BCSpinlockDestroy(&StringPool.lock);
+	for (size_t i = 0; i < BC_STRING_POOL_SIZE; i++) {
+		const StringPoolNode *node = StringPool.buckets[i];
+		while (node) {
+			const StringPoolNode *next = node->next;
+			BCFree(node->str);
+			node = next;
+			if (next == NULL) StringPool.buckets[i] = NULL;
+		}
+	}
 }
 
-static BCStringRef StringPoolGetOrInsert(const char *text, const size_t len,
-                                         const uint32_t hash,
-                                         const BC_bool static_string) {
-  const uint32_t idx = hash % BC_STRING_POOL_SIZE;
+static BCStringRef StringPoolGetOrInsert(
+	const char *text,
+	const size_t len,
+	const uint32_t hash,
+	const BC_bool static_string
+) {
+	const uint32_t idx = hash % BC_STRING_POOL_SIZE;
 
-  BCSpinlockLock(&StringPool.lock);
+	BCSpinlockLock(&StringPool.lock);
 
-  // Lookup
-  const StringPoolNode *node = StringPool.buckets[idx];
-  while (node) {
-    // Fast path for static literal strings
-    if (static_string && node->str->buffer == text) {
-      const BCStringRef ret = (BCStringRef)BCRetain((BCObjectRef)node->str);
-      BCSpinlockUnlock(&StringPool.lock);
-      return ret;
-    }
+	// Lookup
+	const StringPoolNode *node = StringPool.buckets[idx];
+	while (node) {
+		// Fast path for static literal strings
+		if (static_string && node->str->buffer == text) {
+			const BCStringRef ret = (BCStringRef) BCRetain((BCObjectRef) node->str);
+			BCSpinlockUnlock(&StringPool.lock);
+			return ret;
+		}
 
-    if (BC_atomic_load(&node->str->hash) == hash) {
-      // CHECK IF LEN SET
-      if (len != BCStringLength(node->str)) {
-        node = node->next;
-        continue;
-      }
-      if (strcmp(node->str->buffer, text) == 0) {
-        const BCStringRef ret = (BCStringRef)BCRetain((BCObjectRef)node->str);
-        BCSpinlockUnlock(&StringPool.lock);
-        return ret;
-      }
-    }
+		if (BC_atomic_load(&node->str->hash) == hash) {
+			// CHECK IF LEN SET
+			if (len != BCStringLength(node->str)) {
+				node = node->next;
+				continue;
+			}
+			if (strcmp(node->str->buffer, text) == 0) {
+				const BCStringRef ret = (BCStringRef) BCRetain((BCObjectRef) node->str);
+				BCSpinlockUnlock(&StringPool.lock);
+				return ret;
+			}
+		}
 
-    node = node->next;
-  }
+		node = node->next;
+	}
 
-  // ============================================
-  // Insert
-  const size_t extraAlloc = static_string ? sizeof(StringPoolNode)
-                                          : sizeof(StringPoolNode) + (len + 1);
-  const BCStringRef newStr = (BCStringRef)BCObjectAllocWithConfig(
-      NULL, kBCStringClass.id, extraAlloc,
-      static_string ? BC_STRING_FLAG_POOLED | BC_STRING_FLAG_STATIC
-                    : BC_STRING_FLAG_POOLED);
+	// ============================================
+	// Insert
+	const size_t extraAlloc = static_string
+		                          ? sizeof(StringPoolNode)
+		                          : sizeof(StringPoolNode) + (len + 1);
+	const BCStringRef newStr = (BCStringRef) BCObjectAllocWithConfig(
+		NULL,
+		kBCStringClass.id,
+		extraAlloc,
+		static_string
+			? BC_STRING_FLAG_POOLED | BC_STRING_FLAG_STATIC
+			: BC_STRING_FLAG_POOLED
+	);
 
-  if (!static_string) {
-    newStr->buffer = (char *)(&newStr->buffer + 1);
-    memcpy(newStr->buffer, text, len + 1);
-  } else {
-    newStr->buffer = (char *)text;
-  }
+	if (!static_string) {
+		newStr->buffer = (char *) (&newStr->buffer + 1);
+		memcpy(newStr->buffer, text, len + 1);
+	} else {
+		newStr->buffer = (char *) text;
+	}
 
-  newStr->length = len;
-  newStr->hash = hash;
+	newStr->length = len;
+	newStr->hash = hash;
 
-  StringPoolNode *newNode =
-      (StringPoolNode *)((char *)newStr + sizeof(BCString) +
-                         (static_string ? 0 : len + 1));
-  newNode->str = newStr;
-  newNode->next = StringPool.buckets[idx];
-  StringPool.buckets[idx] = newNode;
+	StringPoolNode *newNode =
+			(StringPoolNode *) ((char *) newStr + sizeof(BCString) +
+			                    (static_string ? 0 : len + 1));
+	newNode->str = newStr;
+	newNode->next = StringPool.buckets[idx];
+	StringPool.buckets[idx] = newNode;
 
-  BCSpinlockUnlock(&StringPool.lock);
+	BCSpinlockUnlock(&StringPool.lock);
 
-  newStr->base.ref_count = 0;
+	newStr->base.ref_count = 0;
 
-  return newStr;
+	return newStr;
 }
 
 // =========================================================
@@ -182,38 +188,47 @@ static BCStringRef StringPoolGetOrInsert(const char *text, const size_t len,
 #include "../Core/BCFormat.h"
 
 BCStringRef BCStringCreate(const char *fmt, ...) {
-  va_list args, copy;
-  va_start(args, fmt);
-  va_copy(copy, args);
-  const int len = BC_vsnprintf(NULL, 0, fmt, copy);
-  va_end(copy);
+	va_list args, copy;
+	va_start(args, fmt);
+	va_copy(copy, args);
+	const int len = BC_vsnprintf(NULL, 0, fmt, copy);
+	va_end(copy);
 
-  const BCStringRef str = (BCStringRef)BCObjectAllocWithConfig(
-      NULL, kBCStringClass.id, len + 1, BC_OBJECT_FLAG_REFCOUNT);
-  str->buffer = (char *)(&str->buffer + 1);
+	const BCStringRef str = (BCStringRef) BCObjectAllocWithConfig(
+		NULL,
+		kBCStringClass.id,
+		len + 1,
+		BC_OBJECT_FLAG_REFCOUNT
+	);
+	str->buffer = (char *) (&str->buffer + 1);
 
-  BC_vsnprintf(str->buffer, len + 1, fmt, args);
-  va_end(args);
+	BC_vsnprintf(str->buffer, len + 1, fmt, args);
+	va_end(args);
 
-  str->length = BC_LEN_UNSET;
-  str->hash = BC_HASH_UNSET;
+	str->length = BC_LEN_UNSET;
+	str->hash = BC_HASH_UNSET;
 
-  return str;
+	return str;
 }
 
 BCStringRef BCStringPooled(const char *text) {
-  if (!text)
-    return NULL;
-  return StringPoolGetOrInsert(text, strlen(text),
-                               ___BCINTERNAL___StringHasher(text), BC_false);
+	if (!text) return NULL;
+	return StringPoolGetOrInsert(
+		text,
+		strlen(text),
+		___BCINTERNAL___StringHasher(text),
+		BC_false
+	);
 }
 
-BCStringRef BCStringPooledWithInfo(const char *text, const size_t len,
-                                   const uint32_t hash,
-                                   const BC_bool static_string) {
-  if (!text)
-    return NULL;
-  return StringPoolGetOrInsert(text, len, hash, static_string);
+BCStringRef BCStringPooledWithInfo(
+	const char *text,
+	const size_t len,
+	const uint32_t hash,
+	const BC_bool static_string
+) {
+	if (!text) return NULL;
+	return StringPoolGetOrInsert(text, len, hash, static_string);
 }
 
 // =========================================================
@@ -221,25 +236,23 @@ BCStringRef BCStringPooledWithInfo(const char *text, const size_t len,
 // =========================================================
 
 size_t BCStringLength(const BCStringRef str) {
-  if (!str)
-    return 0;
-  size_t len = BC_atomic_load(&str->length);
-  if (len == BC_LEN_UNSET) {
-    len = strlen(str->buffer);
-    BC_atomic_store(&str->length, len);
-  }
-  return len;
+	if (!str) return 0;
+	size_t len = BC_atomic_load(&str->length);
+	if (len == BC_LEN_UNSET) {
+		len = strlen(str->buffer);
+		BC_atomic_store(&str->length, len);
+	}
+	return len;
 }
 
 uint32_t BCStringHash(const BCStringRef str) {
-  if (!str)
-    return 0;
-  uint32_t hash = BC_atomic_load(&str->hash);
-  if (hash == BC_HASH_UNSET) {
-    hash = ___BCINTERNAL___StringHasher(str->buffer);
-    BC_atomic_store(&str->hash, hash);
-  }
-  return hash;
+	if (!str) return 0;
+	uint32_t hash = BC_atomic_load(&str->hash);
+	if (hash == BC_HASH_UNSET) {
+		hash = ___BCINTERNAL___StringHasher(str->buffer);
+		BC_atomic_store(&str->hash, hash);
+	}
+	return hash;
 }
 
 const char *BCStringCPtr(const BCStringRef str) { return str->buffer; }
@@ -248,78 +261,86 @@ const char *BCStringCPtr(const BCStringRef str) { return str->buffer; }
 // MARK: Debug
 // =========================================================
 
-#define DGRAY "\033[48;5;234m"
-#define BLACK "\033[48;5;235m"
-#define RESET "\033[0m"
-#define BOLD "\033[1m"
+#define CG "\033[48;5;234m"
+#define CB "\033[48;5;235m"
+#define R "\033[0m"
+#define SB "\033[1m"
 
 void BCStringPoolDebugDump(void) {
-  BCSpinlockLock(&StringPool.lock);
-  const clock_t start = clock();
+	BCSpinlockLock(&StringPool.lock);
+	const clock_t start = clock();
 
-  // --------------------------------------------------------------------------
-  // FOOTER
-  printf("\n"
-         "                                          " BOLD
-         "String Pool Dump" RESET "\n"
-         "┌"
-         "────────┬────────────────────────────────────────────────┬───────────"
-         "─┬────────┬──────────────────"
-         "┐\n"
-         "│" BLACK BOLD " Bucket " RESET BLACK "│" BOLD
-         "                     Value                      " RESET BLACK "│" BOLD
-         "    Hash    " RESET BLACK "│" BOLD " Length " RESET BLACK "│" BOLD
-         "       Next       " RESET "│\n"
-         "├" DGRAY "────────┼────────────────────────────────────────────────┼─"
-                   "───────────┼────────┼──────────────────" RESET "┤\n");
+	// --------------------------------------------------------------------------
+	// FOOTER
+	printf(
+		"\n                                          " SB "String Pool Dump" R "\n"
+		"┌────────┬────────────────────────────────────────────────┬────────────┬────────┬──────────────────┐\n"
+		"│" CB SB " Bucket " R CB "│" SB "                     Value                      " R CB "│" SB "    Hash    " R CB "│" SB " Length " R CB "│" SB "       Next       " R "│\n"
+		"├" CG "────────┼────────────────────────────────────────────────┼────────────┼────────┼──────────────────" R "┤\n"
+	);
 
-  // Print entries
-  size_t count = 0;
-  for (size_t i = 0; i < BC_STRING_POOL_SIZE; i++) {
-    const StringPoolNode *node = StringPool.buckets[i];
-    while (node) {
-      const BCStringRef str = node->str;
-      const char *value = str->buffer;
-      const uint32_t hash = BC_atomic_load(&str->hash);
-      const size_t length = BCStringLength(str);
+	// Print entries
+	size_t count = 0;
+	for (size_t i = 0; i < BC_STRING_POOL_SIZE; i++) {
+		const StringPoolNode *node = StringPool.buckets[i];
+		while (node) {
+			const BCStringRef str = node->str;
+			const char *value = str->buffer;
+			const uint32_t hash = BC_atomic_load(&str->hash);
+			const size_t length = BCStringLength(str);
 
-      // Truncate long strings for display
-      char displayValue[47];
-      if (strlen(value) > 43) {
-        snprintf(displayValue, sizeof(displayValue), "%.43s...", value);
-      } else {
-        snprintf(displayValue, sizeof(displayValue), "%s", value);
-      }
+			// Truncate long strings for display
+			char displayValue[47];
+			if (strlen(value) > 43) {
+				snprintf(displayValue, sizeof(displayValue), "%.43s...", value);
+			} else {
+				snprintf(displayValue, sizeof(displayValue), "%s", value);
+			}
 
-      // Format next pointer
-      char nextPtr[18];
-      if (node->next) {
-        snprintf(nextPtr, sizeof(nextPtr), "%p", (void *)node->next);
-      } else {
-        snprintf(nextPtr, sizeof(nextPtr), "NULL");
-      }
+			// Format next pointer
+			char nextPtr[18];
+			if (node->next) {
+				snprintf(nextPtr, sizeof(nextPtr), "%p", (void *)node->next);
+			} else {
+				snprintf(nextPtr, sizeof(nextPtr), "NULL");
+			}
 
-      if (count % 2 == 0)
-        printf("│" BLACK " %-6zu │ %-46s │ 0x%08X │ %-6zu │ %-16s " RESET "│\n",
-               i, displayValue, hash, length, nextPtr);
-      else
-        printf("│" DGRAY " %-6zu │ %-46s │ 0x%08X │ %-6zu │ %-16s " RESET "│\n",
-               i, displayValue, hash, length, nextPtr);
+			if (count % 2 == 0)
+				printf(
+					"│" CB " %-6zu │ %-46s │ 0x%08X │ %-6zu │ %-16s " R "│\n",
+					i,
+					displayValue,
+					hash,
+					length,
+					nextPtr
+				);
+			else
+				printf(
+					"│" CG " %-6zu │ %-46s │ 0x%08X │ %-6zu │ %-16s " R "│\n",
+					i,
+					displayValue,
+					hash,
+					length,
+					nextPtr
+				);
 
-      count++;
-      node = node->next;
-    }
-  }
+			count++;
+			node = node->next;
+		}
+	}
 
-  BCSpinlockUnlock(&StringPool.lock);
+	BCSpinlockUnlock(&StringPool.lock);
 
-  const clock_t end = clock();
-  const double elapsed = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+	const clock_t end = clock();
+	const double elapsed = (double) (end - start) / CLOCKS_PER_SEC * 1000;
 
-  // --------------------------------------------------------------------------
-  // FOOTER
-  printf("└────────┴────────────────────────────────────────────────┴──────────"
-         "──┴────────┴──────────────────┘\n"
-         "    %zu entr%s (%fms)\n\n",
-         count, count == 1 ? "y" : "ies", elapsed);
+	// --------------------------------------------------------------------------
+	// FOOTER
+	printf(
+		"└────────┴────────────────────────────────────────────────┴────────────┴────────┴──────────────────┘\n"
+		"    %zu entr%s (%fms)\n\n",
+		count,
+		count == 1 ? "y" : "ies",
+		elapsed
+	);
 }
